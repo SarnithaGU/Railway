99c0533ace6f1e13421c537d6f772dd329d71d01
SarnithaGU <67185716+SarnithaGU@users.noreply.github.com>
2024-03-28 19:27:29 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Welcome to ZOHO Railways");
+		System.out.println("Welcome to  Railways");
 		printDash();
 		System.out.println("Initialize the Train.. ");
 		System.out.println("Enter the number of cabins in each coach: ");
